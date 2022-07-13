55ba9bb150411e10de122482431e3a1dc198d080
caleb <caleb@sedis.ufrn.br>
2022-07-13 17:57:56 -0300

    wip

---
+++ messagelist.php
@@ -0,0 +1,11 @@
+<?php
+require_once '../../../config.php';
+global $OUTPUT;
+require_capability('moodle/site:sendmessage', context_system::instance());
+
+echo $OUTPUT->header();
+
+
+
+echo $OUTPUT->header();
+?>
--- settings.php
+++ settings.php
@@ -1,10 +1,11 @@
 <?php
 defined('MOODLE_INTERNAL') || die;
+global $CFG;
 
-if (has_capability('moodle/course:create', context_system::instance())) {
-    if (!$ADMIN->locate('tool_auditorireport')) {
-        $ADMIN->add('courses',
-            new admin_externalpage('tool_auditorireport', get_string('pluginname', 'tool_auditorireport'),
-                $CFG->wwwroot . '/' . $CFG->admin . '/tool/auditorireport/index.php'));
+if (has_capability('moodle/site:sendmessage', context_system::instance())) {
+    if (!$ADMIN->locate('tool_automessage')) {
+        $ADMIN->add('accounts',
+            new admin_externalpage('tool_automessage', get_string('pluginname', 'tool_auditorireport'),
+                $CFG->wwwroot . '/' . $CFG->admin . '/tool/automessage/messagelist.php'));
     }
 }
